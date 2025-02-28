d5022559d9d0fbe39224e89be1af470a8a190828
DanielLópez-ITB2425 <daniel.lopez.7e8@itb.cat>
2025-02-28 08:39:57 +0100

    Add files via upload

---
+++ E04/js/funcionextra.js
@@ -0,0 +1,14 @@
+//Funciones extra
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Detectar cambios de visibilidad
+    document.addEventListener("visibilitychange", function() {
+        if (document.hidden) {
+            // Cambiar el título cuando la página no es visible
+            document.title = "¡No te vayas!:(";
+        } else {
+            // Restaurar el título original cuando la página vuelve a ser visible
+            document.title = "Calculadora😤";
+        }
+    });
+});
